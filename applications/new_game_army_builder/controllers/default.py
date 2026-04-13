14aba397c4659142dff3b6bf73ff6cc89235b6ad
barry4356 <barryd@mail.usf.edu>
2026-04-13 19:32:14 +0000

    Initial Layout Started

--- applications/new_game_army_builder/controllers/default.py
+++ applications/new_game_army_builder/controllers/default.py
@@ -7,8 +7,34 @@ import os
 import json
 # ---- example index page ----
 armyData = []
+armyDataFiltered = []
+QualityOptions = [2,3,4,5,6,7,8,9,10]
+DefenseOptions = [2,3,4,5,6,7,8,9,10]
+
 def index():
     dataFilePath = os.path.join(request.folder, 'private', 'AllArmyData.json')
     with open(dataFilePath, 'r') as dataFile:
         armyData = json.load(dataFile)
-    return dict(loaded=True)
+        armyDataFiltered = armyData.copy()
+    return dict(loaded=True, myQualityOptions=QualityOptions, myDefenseOptions=DefenseOptions)
+
+
+def select_handler():
+    # Get the selected value from request.vars
+    #selected_value = request.vars.value
+    # Perform backend logic
+    #result = "You selected: " + selected_value
+    DefenseOptions = [3,4]
+    return request.vars.qual # Or return a component/json
+
+
+
+
+
+
+
+
+
+
+
+
