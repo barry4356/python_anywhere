298b904c7db2c22927c5f5b58e2115708a451029
Barry Dunham <dunhambarrya@gmail.com>
2026-04-16 05:09:38 -0400

    Add constants file

---
+++ applications/new_game_army_builder/modules/constants.py
@@ -0,0 +1,15 @@
+NEW_CAF_UNIT={
+    'Model Qty': -1,
+    'Quality': -1,
+    'Defense': -1,
+    'Weapons': [],
+    'Cost': -1,
+    'Special Perks': [],
+}
+
+NEW_CAF_WEAPON={
+    'Weapon Perks': [],
+    'Weapon Range': -1,
+    'Weapon qty per model': -1,
+    'Quantity': -1
+}
