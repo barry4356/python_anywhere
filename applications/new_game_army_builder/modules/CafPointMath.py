239e90479e7ebcc4192920b7c3925d9ad1fae0fa
Barry Dunham <dunhambarrya@gmail.com>
2026-04-19 07:22:27 -0400

    Add Rending, Regen, and Fast math

--- applications/new_game_army_builder/modules/CafPointMath.py
+++ applications/new_game_army_builder/modules/CafPointMath.py
@@ -28,17 +28,77 @@ def _calculateBaseCost(CafUnit):
 
 
 def _calculateModelPerksCost(CafUnit, base_cost):
+    perk_cost = 0
     #Price of Perks associated with the models
-    return 0
+    if 'Specs' in OprUnit.keys() and 'Fast' in OprUnit['Specs']:
+        perk_cost += .333333 * base_cost
+    if 'Specs' in OprUnit.keys() and 'Regen' in OprUnit['Specs']:
+        perk_cost += 2.47 * OprUnit["ModelCount"]
+    return perk_cost
 
 def _calculateLoadoutCost(CafUnit):
     #Price of all weapons in unit
     loadoutCost = 0
-    #for weapon in CafUnit['weapons']:
-    #    loadoutCost += __caclulateWeaponCost(CafWeapon)
+    for weapon in OprUnit['Weapons']:
+        #Currently; all models in a unit must share the same weapon(s)
+        loadoutCost += (__caclulateWeaponCost(weapon) * OprUnit["ModelCount"])
     return loadoutCost
 
 def __caclulateWeaponCost(CafWeapon, quality):
+    qua_pts = 11 - quality #Convert Qual to a simple scalar
+    qua_pts = qua_pts / 10 # And then adjust it based on dice %
+    #OPR Uses linear scaling for cost until >66.6(repeating, of course)%, and then switches to exponential scaling
+    if qua_pts <= .666:
+        price_per_attack = qua_pts * 7.8 #Derived from OPR Army Stats/Cost
+    else:
+        #Quadratic formula derived from OPR Armies (https://www.omnicalculator.com/statistics/quadratic-regression)
+        price_per_attack = 7.8-(19.5 * qua_pts)+(23.4*qua_pts*qua_pts)
+        price_per_attack_base = price_per_attack
+    rending = False
+    if 'Rending' in OprWeapon.keys() and OprWeapon['Rending']:
+        rending = True
+    if 'AP' in OprWeapon.keys():
+        #AP/Rending math is.... weird. Simplified by breaking into the 4 possible cases based on AP level
+        #AP Adjusts at a consistent ratio (regardless of quality); Rending follows the 'linear until 66% quality, and then exponential' paradigm
+        try:
+            ap_val = int(OprWeapon["AP"])
+            if ap_val == 1:
+                price_per_attack *= 1.5
+                if rending:
+                    if qua_pts <= .666:
+                        price_per_attack += 0.65 + (1.812 * qua_pts)
+                    else:
+                        2.861 - (4.551 * qua_pts) + (5.454 * qua_pts * qua_pts)
+            elif ap_val == 2:
+                price_per_attack *= 2
+                if rending:
+                    if qua_pts <= .666:
+                        price_per_attack += 0.6495 + (1.1664 * qua_pts)
+                    else:
+                        1.815 - (2.91 * qua_pts) + (3.492 * qua_pts * qua_pts)
+            elif ap_val == 3:
+                price_per_attack *= 2.4
+                if rending:
+                    if qua_pts <= .666:
+                        price_per_attack += 0.65 + (0.648 * qua_pts)
+                    else:
+                        1.292 - (1.599 * qua_pts) + (1.926 * qua_pts * qua_pts)
+            elif ap_val == 4:
+                price_per_attack *= 2.7
+                if rending:
+                    if qua_pts <= .666:
+                        price_per_attack += 0.6495 + (0.2598 * qua_pts)
+                    else:
+                        0.899 - (0.618 * qua_pts) + (0.756 * qua_pts * qua_pts)
+        except:
+            print("ERROR: WTF?")
+    else:
+        #Rending base case (NO AP present)
+        if qua_pts <= .666:
+            price_per_attack += 0.6505 + (2.2002 * qua_pts)
+        else:
+            2.861 - (5.532 * qua_pts) + (6.624 * qua_pts * qua_pts)
     #Price of a single weapon for a single model in unit
-    
-    return 0
+    weapon_price = price_per_attack * OprWeapon["Attacks"]
+    return weapon_price
+
